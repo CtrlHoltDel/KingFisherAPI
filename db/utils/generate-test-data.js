367fb076cc76bdd7ad5fd4cb5c4eaedde25757d0
CtrlHoltDel <CtrlHoltDel@gmail.com>
2022-11-02 06:03:29 +0000

    Added generating of test data

---
+++ db/utils/generate-test-data.js
@@ -0,0 +1,22 @@
+const generateUUID = require("../../utils/UUID");
+const db = require("../connection");
+
+const insert = async () => {
+    console.log(process.env);
+
+    const { TYPE, PLAYER_ID, NOTE_AMOUNT = 50, NOTE_PREFIX = 'note' } = process.env;
+
+    if(TYPE === 'note'){        
+        for (let i = 0; i < NOTE_AMOUNT; i++) {
+            await db.query(`INSERT INTO notes(id, created_by, note, player_id, type) VALUES ($1, $2, $3, $4, $5)`, [generateUUID(), 'ctrlholtdel', `${NOTE_PREFIX} ${i}`, PLAYER_ID, 'note'])
+        }
+    
+        for (let i = 0; i < NOTE_AMOUNT; i++) {
+            await db.query(`INSERT INTO notes(id, created_by, note, player_id, type) VALUES ($1, $2, $3, $4, $5)`, [generateUUID(), 'ctrlholtdel', `${NOTE_PREFIX} ${i}`, '1', 'tendency'])
+        }
+    }
+}
+
+
+
+insert()
